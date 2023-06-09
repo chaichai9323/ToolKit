17e10902d4961c071fa991ff5a77fcb8a7a74f4e
chailintao <chailintao@laien.io>
2023-06-09 10:03:18 +0800

    配置脚本完成

--- exe.sh
+++ exe.sh
@@ -2,15 +2,76 @@
 
 file=AppStoreEnv.rb
 
-if [ ! -f "$file" ]; then
-	url=https://raw.githubusercontent.com/chaichai9323/ToolKit/main/AppStoreEnv.rb
-	curl -fsSL ${url} -o $file
-fi
+cat > ${file} <<- 'EOF'
+#!/usr/bin/ruby 
 
-if [ ! -f "$file" ]; then
-	echo -e "\033[31m执行脚本出错了，请检查网络，然后重新执行\033[0m"; exit 0
+script = 'branchName=`git rev-parse --abbrev-ref HEAD`
+productBranch=false
+if [ $branchName == master ] || [ $branchName == main ]; then
+    productBranch=true
+    echo "当前在[master/main]分支"
+else
+    echo "当前在[$branchName]分支"
 fi
+if [ $productBranch == true ]; then
+    echo "准备开始校验环境"
+    if [[ $SWIFT_ACTIVE_COMPILATION_CONDITIONS =~ "AppStoreEnv" ]]; then
+        echo "校验环境通过"
+    else
+        echo "error:校验环境不通过，请检查配置"
+        exit 1
+    fi
+fi'
 
+script2='googlepath=""
+for f in `find $SRCROOT -name "GoogleService-Info*.plist"`; do
+  content=`grep -E "test-project.*\.appspot\.com" $f`
+  if [[ $content == *appspot* ]]; then
+    continue
+  else
+    googlepath=$f
+    break
+  fi
+done
+branchName=`git rev-parse --abbrev-ref HEAD`
+if [ $branchName == master ] || [ $branchName == main ]; then
+  if [[ ${#googlepath} > 0 ]]; then
+    echo "找到正式服:"$googlepath
+    dstPath=$TARGET_BUILD_DIR/$TARGET_NAME.app/GoogleService-Info.plist
+    cp -f $googlepath $dstPath
+  else
+    echo "error:没有找到正式服GoogleInfo"
+    exit 1
+  fi
+fi
+'
+script_phase :name => 'verifyAppStoreEnv', :script => "#{script}", :execution_position => :before_compile
+script_phase :name => 'verifyGoogleInfo', :script => "#{script2}", :execution_position => :after_compile
+
+branchName = `git rev-parse --abbrev-ref HEAD`.gsub(/\s+/, '')
+condition = ""
+if branchName == "master" || branchName == "main"
+  condition = "AppStoreEnv"
+else
+  condition = "DevelopEnv"
+end
+
+post_integrate do |installer|
+  installer.generated_aggregate_targets.each do |target|
+    Dir.glob("Pods/**/*#{target.name}*.xcconfig").each do | fold |
+      content = File.read("#{fold}").gsub!(/(SWIFT_ACTIVE_COMPILATION_CONDITIONS){1}\W*=/,"\\1 = $(inherited) #{condition} ")
+      if content == nil
+        str = "SWIFT_ACTIVE_COMPILATION_CONDITIONS = $(inherited) #{condition}"
+        File.open("#{fold}", "a") do |file|
+          file.write(str)
+        end
+      else
+        File.write("#{fold}", content)    
+      end
+    end
+  end
+end  
+EOF
 
 comstr="if File.exist?('./AppStoreEnv.rb');eval File.read('./$file');end"
 if [[ `cat Podfile` =~ "$comstr" ]]; then
