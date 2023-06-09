a2f66906fc2b92f41878db9eea0ea05c3384d5b8
chailintao <chailintao@laien.io>
2023-06-09 11:32:24 +0800

    fix

--- exe.sh
+++ exe.sh
@@ -15,7 +15,7 @@ else
 fi
 cfg=$(echo $CONFIGURATION | tr [A-Z] [a-z])
 if [ $productBranch == true ] && [ $cfg == release ]; then
-    echo "准备开始校验环境"
+    echo "准备开始校验$cfg环境"
     if [[ $SWIFT_ACTIVE_COMPILATION_CONDITIONS =~ "AppStoreEnv" ]]; then
         echo "校验环境通过"
     else
@@ -36,7 +36,7 @@ for f in `find $SRCROOT -name "GoogleService-Info*.plist"`; do
 done
 branchName=`git rev-parse --abbrev-ref HEAD`
 cfg=$(echo $CONFIGURATION | tr [A-Z] [a-z])
-if ([ $branchName == master ] || [ $branchName == main ]) && [ $cfk == release ]; then
+if ([ $branchName == master ] || [ $branchName == main ]) && [ $cfg == release ]; then
   if [[ ${#googlepath} > 0 ]]; then
     echo "找到正式服:"$googlepath
     dstPath=$TARGET_BUILD_DIR/$TARGET_NAME.app/GoogleService-Info.plist
